4f7c419c88ee89105f4e5bd178de3cd5ceda9cee
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2025-06-20 20:03:47 +0100

    added a new component to test if i can edit already deployed app

--- src/Screen/Home.tsx
+++ src/Screen/Home.tsx
@@ -1,6 +1,7 @@
 import Header from '../Components/Header'
 import Hero from '../Components/Hero'
 import GenreMovieList from '../Components/GenreMovieList'
+import Test from '../Components/Test'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
      <Header />
      <Hero />
      <GenreMovieList />
+     <Test />
     </>
    
   )
